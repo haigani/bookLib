2d9713b31dc935579df275b782213cdd6ad30967
Hanieh Aigani <haniye_aygani@yahoo.com>
2024-10-05 19:29:08 -0400

    urls settings.

---
+++ books/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import BookDetailView, BookListCreateView
+
+urlpatterns = [
+    path('books/', BookListCreateView.as_view(), name="book_list_create"),
+    path('books/<int:pk>/', BookDetailView.as_view(), name="book_detail")
+]
