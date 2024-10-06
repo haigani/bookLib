0551f94d52f8564a9f5239979fdc45b9b2d5607e
Hanieh Aigani <haniye_aygani@yahoo.com>
2024-10-05 22:47:22 -0400

    filter-search implementations.

--- books/views.py
+++ books/views.py
@@ -2,12 +2,17 @@ from rest_framework import generics, permissions
 
 from .models import Book
 from .serializers import BookSerializer, UserSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 
 
 class BookListCreateView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['title']
+    search_fields = ['title']
 
 
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
