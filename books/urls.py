61cba60c65eadeeb67d1d9b48fb1006f17b7b27d
Hanieh Aigani <haniye_aygani@yahoo.com>
2024-10-05 20:51:21 -0400

    creating the login urls based on django existing loginView(not customized).

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import BookDetailView, BookListCreateView
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('books/', BookListCreateView.as_view(), name="book_list_create"),
     path('books/<int:pk>/', BookDetailView.as_view(), name="book_detail"),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
